570afa3769e7ccd4972f7237cfc46bf56df65afe
Hutzel-Levente-30021 <57431183+Hutzel-Levente-30021@users.noreply.github.com>
2021-04-24 11:00:40 +0300

    Delete Laborator4 directory

--- Laborator4/Exercitiu5/test/compression.js
+++
@@ -1,9 +0,0 @@
-const lib = require('bits-utils');
-
-const text = 'IT IS BETTER LATTTTER THAN NEVER';
-// console.log(text.length);
-const symbols = lib.countASCIISymbols(text);
-const alphabet = lib.getAlphabet(symbols);
-const codes = lib.getSymbolsBitCodes(alphabet, 4)
-console.log(codes);
-
