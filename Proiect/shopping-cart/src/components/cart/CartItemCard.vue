c5200e76522cffa576d415ba4a9a968936d152b2
Hutzel-Levente-30021 <57431183+Hutzel-Levente-30021@users.noreply.github.com>
2021-05-05 17:23:41 +0300

    Add files via upload

---
+++ Proiect/shopping-cart/src/components/cart/CartItemCard.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="cart-item-card" v-if="product.quantity>0">
+        <div class="header">
+            <h3>{{ product.title }}</h3>
+            <h4>În coș: {{ product.quantity }}</h4>
+            <h4>Total: {{ item_cost.toFixed(2) }}</h4>
+        </div>
+
+        <p>{{ description }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['product'],
+    computed: {
+        description(){
+            return this.product.description
+        },
+        item_cost(){
+            return this.product.price * this.product.quantity
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.cart-item-card{
+    width: 90%;
+    margin: 5px;
+    background-color: white;
+    box-shadow: 0 0 5px gray;
+    border-radius: 5px;
+    padding: 10px;
+    text-align: left;
+
+    .header{
+        display: flex;
+        justify-content: space-around;
+    }
+}
+</style>
---
+++ Proiect/shopping-cart/src/components/cart/CartSummaryPaymentCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="cart-item-card">
+        <h3>Total: {{ cart_total.toFixed(2) }} lei</h3>
+
+        <button class="view-product-button">Plătește cu card</button>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        cart_total() {
+            return this.$store.getters.cartTotal
+        }
+    }
+}
+</script>
