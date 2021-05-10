e918a19cf04592c87f12a51234d174269b21edd1
Hutzel-Levente-30021 <levente.hutzel16@gmail.com>
2021-05-10 11:05:09 +0300

    Proiect update

--- Proiect/shopping-cart/src/views/About.vue
+++ Proiect/shopping-cart/src/views/About.vue
@@ -4,7 +4,7 @@
     <p>Hutzel Levente</p>
     <p>Adresă: Str. <span style="filter:blur(5px)">Prahovei</span> nr. <span style="filter:blur(5px)">7.</span>, Mun. Satu Mare, jud. Satu Mare</p>
     <p>Tel: +40<span style="filter:blur(5px)">758704362</span></p>
-    <p>Adresă e-mail: levente.hutzel16@gmail.com</p>
+    <p>Adresă e-mail: <a href="mailto:levente.hutzel16@gmail.com">levente.hutzel16@gmail.com</a></p>
   </div>
 </template>
 
